d291aaea208ed441d93cfffc0c2a583c758fb56b
Dealer86 <adicroitor86@yahoo.com>
2023-05-25 20:14:11 +0300

    Add get_by_id method to repo

--- API_which_can_save_users/domain/repo.py
+++ API_which_can_save_users/domain/repo.py
@@ -3,6 +3,10 @@ from API_which_can_save_users.domain.user_factory import UserFactory
 from API_which_can_save_users.persistence.users_sqlite import UsersSqlLite
 
 
+class InvalidUserId(Exception):
+    pass
+
+
 class UserRepository:
     def __init__(self):
         self.__users_db = UsersSqlLite("persistence/users.db")
@@ -13,12 +17,19 @@ class UserRepository:
         self.__load_from_db()
 
     def get_all(self) -> list[User]:
+        print([u for u in self.__users if u.id == 2])
         return self.__users
 
     def delete(self, id_: int):
         self.__users_db.delete(id_)
         self.__users = [u for u in self.__users if u.id != id_]
 
+    def get_by_id(self, id_: int) -> User:
+        user_tuple = self.__users_db.get_by_id(id_)
+        if user_tuple is None:
+            raise InvalidUserId("User id does not exist")
+        our_user = UserFactory.create_from_db(user_tuple)
+        return our_user
+
     def __load_from_db(self):
         self.__users = [UserFactory.create_from_db(u) for u in self.__users_db.get_all()]
-
