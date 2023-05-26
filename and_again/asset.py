e1b3477e1a33eddb921e18e40ba185430cadf241
Dealer86 <adicroitor86@yahoo.com>
2023-05-26 22:56:47 +0300

    Add another ex with no duplicate asset allowed

---
+++ and_again/asset.py
@@ -0,0 +1,25 @@
+class Asset:
+    def __init__(self, ticker: str, price: int, country: str):
+        self.__ticker = ticker
+        self.__price = price
+        self.__country = country
+
+    @property
+    def ticker(self):
+        return self.__ticker
+
+    @property
+    def price(self):
+        return self.__price
+
+    @property
+    def country(self):
+        return self.__country
+
+    def to_dict(self) -> dict:
+        return {"ticker": self.ticker, "price": self.price, "country": self.country}
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        return Asset(ticker=d["ticker"], price=d["price"], country=d["country"])
+
