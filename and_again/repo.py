bbd7acd93f846ae4f6f4bb3b0eabed6728f4c6c5
Dealer86 <adicroitor86@yahoo.com>
2023-05-26 22:56:47 +0300

    Add another ex with no duplicat asset allowed

---
+++ and_again/repo.py
@@ -0,0 +1,56 @@
+import json
+import os
+
+from and_again.asset import Asset
+from and_again.uzar import User
+
+
+class UserFile:
+    def __init__(self, file_path):
+        self.__file_path = file_path
+
+    def get_all(self) -> list[User]:
+        if not os.path.exists(self.__file_path):
+            return []
+        try:
+            with open(self.__file_path) as f:
+                json_content = f.read()
+                content_objects = json.loads(json_content)
+                return [User.from_dict(u) for u in content_objects]
+        except Exception as e:
+            print(str(e))
+            raise e
+
+    def add_asset_to_user(self, user: User, asset: Asset):
+        user_list = self.get_all()
+        for u in user_list:
+            if u.id_ == user.id_ and (asset.ticker not in [a.ticker for a in u.asset]):
+                print(asset.ticker)
+                print([a.ticker for a in u.asset])
+                u.asset.append(asset)
+            else:
+                print("Asset already in list")
+        self.__save_to_file(user_list)
+
+    def add(self, user: User):
+        user_list = self.get_all()
+        user_list.append(user)
+        self.__save_to_file(user_list)
+
+    def __save_to_file(self, user_list: list[User]):
+        data = [user.to_dict() for user in user_list]
+        json_prep = json.dumps(data, indent=4)
+        with open(self.__file_path, "w") as f:
+            f.write(json_prep)
+
+
+
+if __name__ == "__main__":
+    user1 = User(1, "adi")
+    repo = UserFile("uzar.json")
+    # repo.add(user1)
+    asset1 = Asset("Mar", 100, "ROMANIA")
+    repo.add_asset_to_user(user1, asset1)
+
+
+
---
+++ and_again/uzar.py
@@ -0,0 +1,30 @@
+from and_again.asset import Asset
+
+
+class User:
+    def __init__(self, id_: int, name: str, asset: list[Asset] = None):
+        self.__id_ = id_
+        self.__name = name
+        self.__asset = asset if asset else []
+
+    @property
+    def id_(self):
+        return self.__id_
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def asset(self):
+        return self.__asset
+
+    def to_dict(self) -> dict:
+        asset_dict = [u.to_dict() for u in self.asset]
+        return {"id_": self.id_, "name": self.name, "asset": asset_dict}
+
+    @classmethod
+    def from_dict(cls, d: dict):
+        return User(id_=d["id_"], name=d["name"], asset=[Asset.from_dict(a) for a in d["asset"]])
+
+
