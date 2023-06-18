3d36066cbfab90fcb369efe3e9be9836d4998f0c
Dealer86 <adriancroitor.tech@gmail.com>
2023-06-18 23:05:04 +0300

    nasa request

--- api_requests/nasa_api_requests.py
+++ api_requests/nasa_api_requests.py
@@ -1,13 +1,34 @@
+import os
+
 import requests
 api_key = "DEMO_KEY"
 
 url = "https://api.nasa.gov/planetary/apod"
 
-response = requests.get(url, params={"api_key": api_key, "date": "2020-08-17"})
+# response = requests.get(url, params={"api_key": api_key, "date": "2020-08-17"})
+# print(response)
+# body = response.json()
+# print(body.keys())
+# print(body["hdurl"])
+# print(body["explanation"])
+
+response = requests.get(url, params={'api_key': api_key, 'date': "2023-02-27"})
 print(response)
-body = response.json()
-print(body.keys())
-print(body["hdurl"])
-print(body["explanation"])
+print(response.links)
+print(response.json())
+x = response.json()
+print(x["hdurl"])
+# exp = x["explanation"].split(".")
+# for a in exp:
+#     print(a)
+
+
+
+
+
+
+
+
+
 
 
